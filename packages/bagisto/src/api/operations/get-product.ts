584199b46b1fdaf520a38670824b0b54924dd487
Devansh <devansh.bawari419@webkul.com>
2022-02-21 18:47:05 +0530

    Specific Product Handled

--- packages/bagisto/src/api/operations/get-product.ts
+++ packages/bagisto/src/api/operations/get-product.ts
@@ -2,14 +2,14 @@ import { Product } from '@vercel/commerce/types/product'
 import { GetProductOperation } from '@vercel/commerce/types/product'
 import type { OperationContext } from '@vercel/commerce/api/operations'
 import type { BagistoCommerceConfig } from '../index'
-import { getProductQuery } from '../utils/graphql-api/queries/get-product-query'
+import { getAllProductsQuery } from '../utils/graphql-api/queries/get-all-products-query'
 import { normalizeProduct } from '../lib/normalize'
 
 export default function getProductOperation({
   commerce,
 }: OperationContext<any>) {
   async function getProduct<T extends GetProductOperation>({
-    query = getProductQuery,
+    query = getAllProductsQuery,
     variables,
     config,
   }: {
@@ -18,19 +18,20 @@ export default function getProductOperation({
     config?: Partial<BagistoCommerceConfig>
     preview?: boolean
   } = {}): Promise<Product | {} | any> {
-    const productVariables = { slug: variables?.slug }
-
     const bagistoConfig = commerce.getConfig(config)
 
     const data = await bagistoConfig.fetch(query)
 
-    const normalizedProduct = normalizeProduct(
-      data.data.products.data[0],
-      bagistoConfig
-    )
+    const normalizedProducts = data.data.newProducts
+      ? data.data.newProducts.map((item: any) =>
+          normalizeProduct(item, bagistoConfig)
+        )
+      : []
 
     return {
-      product: normalizedProduct,
+      product: normalizedProducts.find(
+        ({ slug }: any) => slug === variables!.slug
+      ),
     }
   }
 
--- packages/bagisto/src/api/utils/graphql-api/queries/get-all-products-query.ts
+++ packages/bagisto/src/api/utils/graphql-api/queries/get-all-products-query.ts
@@ -1,19 +1,19 @@
 export const getAllProductsQuery = `
 query newProducts {
   newProducts (count: 6) {
-      productFlats {
-        id
-        productId
-        name
-        urlKey
-        price
-        description
-      }
-      images {
-        id
-        path
-        url
-      }
+    productFlats {
+      id
+      productId
+      name
+      urlKey
+      price
+      description
+    }
+    images {
+      id
+      path
+      url
+    }
   }
 }
 `
