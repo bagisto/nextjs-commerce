861a3cb27def32a83daec779a7bd65647b220540
Devansh <devansh.bawari419@webkul.com>
2022-03-04 19:36:46 +0530

    Fragment Added

--- packages/bagisto/src/api/queries/get-all-products-query.ts
+++ packages/bagisto/src/api/queries/get-all-products-query.ts
@@ -1,3 +1,5 @@
+import { productInfoFragment } from '../fragments/product'
+
 export const getAllProductsQuery = /* GraphQL */ `
   query getProductListing(
     $input: FilterProductListingInput
@@ -12,110 +14,10 @@ export const getAllProductsQuery = /* GraphQL */ `
         total
       }
       data {
-        id
-        type
-        isInWishlist
-        attributeFamilyId
-        sku
-        parentId
-        superAttributes {
-          id
-          code
-          adminName
-          type
-          position
-        }
-        configutableData {
-          index {
-            id
-            attributeOptionIds {
-              attributeId
-              attributeOptionId
-            }
-          }
-          attributes {
-            id
-            code
-            label
-            options {
-              id
-              label
-            }
-          }
-        }
-        productFlats {
-          id
-          sku
-          productNumber
-          name
-          description
-          shortDescription
-          urlKey
-          new
-          featured
-          status
-          visibleIndividually
-          thumbnail
-          price
-          cost
-          specialPrice
-          specialPriceFrom
-          specialPriceTo
-          weight
-          color
-          colorLabel
-          size
-          sizeLabel
-          locale
-          channel
-          productId
-          parentId
-          minPrice
-          maxPrice
-          metaTitle
-          metaKeywords
-          metaDescription
-          width
-          height
-          depth
-          createdAt
-          updatedAt
-        }
-        cacheBaseImage {
-          smallImageUrl
-          mediumImageUrl
-          largeImageUrl
-          originalImageUrl
-        }
-        cacheGalleryImages {
-          smallImageUrl
-          mediumImageUrl
-          largeImageUrl
-          originalImageUrl
-        }
-        priceHtml {
-          id
-          type
-          html
-          regular
-          regularWithoutCurrencyCode
-          special
-          specialWithoutCurrencyCode
-          currencyCode
-        }
-        reviews {
-          id
-          title
-          rating
-          comment
-          status
-          productId
-          customerId
-          customerName
-          createdAt
-          updatedAt
-        }
+        ...productInfo
       }
     }
   }
+
+  ${productInfoFragment}
 `
--- packages/bagisto/src/api/queries/get-featured-products-query.ts
+++ packages/bagisto/src/api/queries/get-featured-products-query.ts
@@ -1,102 +1,11 @@
+import { productInfoFragment } from '../fragments/product'
+
 export const getFeaturedProductsQuery = /* GraphQL */ `
   query featuredProducts {
     featuredProducts {
-      id
-      type
-      isInWishlist
-      attributeFamilyId
-      sku
-      parentId
-      configutableData {
-        index {
-          id
-          attributeOptionIds {
-            attributeId
-            attributeOptionId
-          }
-        }
-        attributes {
-          id
-          code
-          label
-          options {
-            id
-            label
-          }
-        }
-      }
-      productFlats {
-        id
-        sku
-        productNumber
-        name
-        description
-        shortDescription
-        urlKey
-        new
-        featured
-        status
-        visibleIndividually
-        thumbnail
-        price
-        cost
-        specialPrice
-        specialPriceFrom
-        specialPriceTo
-        weight
-        color
-        colorLabel
-        size
-        sizeLabel
-        locale
-        channel
-        productId
-        parentId
-        minPrice
-        maxPrice
-        metaTitle
-        metaKeywords
-        metaDescription
-        width
-        height
-        depth
-        createdAt
-        updatedAt
-      }
-      cacheBaseImage {
-        smallImageUrl
-        mediumImageUrl
-        largeImageUrl
-        originalImageUrl
-      }
-      cacheGalleryImages {
-        smallImageUrl
-        mediumImageUrl
-        largeImageUrl
-        originalImageUrl
-      }
-      priceHtml {
-        id
-        type
-        html
-        regular
-        regularWithoutCurrencyCode
-        special
-        specialWithoutCurrencyCode
-        currencyCode
-      }
-      reviews {
-        id
-        title
-        rating
-        comment
-        status
-        productId
-        customerId
-        customerName
-        createdAt
-        updatedAt
-      }
+      ...productInfo
     }
   }
+
+  ${productInfoFragment}
 `
