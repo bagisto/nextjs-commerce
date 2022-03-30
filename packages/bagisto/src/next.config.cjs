0351cb31c14ccbc46828161fa362437346e23f84
Devansh <devansh.bawari419@webkul.com>
2022-03-30 17:09:02 +0530

    Testing

--- packages/bagisto/src/next.config.cjs
+++ packages/bagisto/src/next.config.cjs
@@ -3,6 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['192.168.15.114'],
+    domains: ['192.168.15.114', '28fd-115-113-191-18.ngrok.io'],
   },
 }
