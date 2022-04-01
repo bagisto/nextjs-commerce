ebd03c53e9b746eeed4b365bf4eec93628ee317d
Devansh <devansh.bawari419@webkul.com>
2022-04-01 13:11:07 +0530

    Added Image Domain

--- packages/bagisto/src/next.config.cjs
+++ packages/bagisto/src/next.config.cjs
@@ -3,6 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['192.168.15.114', '28fd-115-113-191-18.ngrok.io'],
+    domains: ['localhost', '192.168.15.114', 'bagistotest.tk'],
   },
 }
