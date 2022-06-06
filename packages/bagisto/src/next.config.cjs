e788bd7225a9057b6460ec9948ef4670a9384ac1
Devansh <devansh.bawari419@webkul.com>
2022-06-06 16:50:44 +0530

    Updated URL

--- packages/bagisto/src/next.config.cjs
+++ packages/bagisto/src/next.config.cjs
@@ -3,6 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['localhost', '192.168.15.114', 'bagistotest.tk'],
+    domains: ['demo.bagisto.com'],
   },
 }
