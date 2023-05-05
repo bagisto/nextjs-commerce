278be94fad01ae7a33c25bc2f189184ab2274227
Vikas Tiwari <vikastiwari.magento160@webkul.in>
2023-05-05 17:21:33 +0530

    refactor: added domain to images domain next config

--- packages/bagisto/src/next.config.cjs
+++ packages/bagisto/src/next.config.cjs
@@ -3,6 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['d146-182-75-44-194.ngrok-free.app'],
+    domains: ['nextjs.bagisto.com'],
   },
 }
