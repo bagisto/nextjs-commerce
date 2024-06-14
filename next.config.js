c470cfc87572a2b17a7b9409f21a5afb4d50cf2a
abhijit kumar <abhijit.kumar018@webkul.in>
2024-06-14 19:55:30 +0530

    fix : compress

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  compress:true, 
   eslint: {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true
